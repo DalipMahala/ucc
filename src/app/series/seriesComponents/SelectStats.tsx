7e001e56ea18e9118de216ffd101bb5e121e84eb
DalipMahala <okey.dalip@gmail.com>
2025-05-26 10:58:51 +0530

    newCode86

--- src/app/series/seriesComponents/SelectStats.tsx
+++ src/app/series/seriesComponents/SelectStats.tsx
@@ -7,14 +7,15 @@ const StateSelector = () => {
   const router = useRouter();
   const pathname = usePathname();
 
+  let parts = pathname.split('/');
+
+  let defaultStats = parts[parts.length - 1];
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const statsType = e.target.value;
-
+    defaultStats = statsType;
 
     if (statsType) {
-        const parts = pathname.split('/');
         parts[parts.length - 1] = statsType;
-
         const newPath = parts.join('/');
         console.log(newPath);
       router.push(newPath);
@@ -26,7 +27,7 @@ const StateSelector = () => {
     <select
     className="border-[1px] rounded-md p-2 bg-[#ffffff]"
     onChange={handleChange}
-    defaultValue="batting-most-run"
+    defaultValue={defaultStats}
     >
     <option value="batting-most-run">Most Runs</option>
     <option value="batting-highest-average">
@@ -42,6 +43,7 @@ const StateSelector = () => {
     <option value="bowling-most-wicket">Most Wickets</option>
     <option value="bowling-best-average">Best Bowling Average</option>
     <option value="bowling-best-figures">Best Bowling</option>
+    <option value="bowling-most-five-wicket-hauls">Most 5 Wickets Haul</option>
     <option value="bowling-best-economy-rates">Best Economy</option>
     <option value="bowling-best-strike">
         Best Bowling Strike Rate
